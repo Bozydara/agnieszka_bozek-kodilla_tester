9863d2a2fc82eeb88ba22e160e012bf9732eb404
Agnieszka Bozek <a.bozek90@gmail.com>
2025-01-09 22:14:06 +0100

    Task with variables

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String shopName = "Tulip";
+        String shopOwner = "Benjamin Kezob";
+        int numberOfEmployees = 2000;
+        double googleRating = 4.2;
+        char shopCategory = 'W';
+        boolean isClosed = true;
+
+        System.out.println(shopName);
+        System.out.println(shopOwner);
+        System.out.println(numberOfEmployees);
+        System.out.println(googleRating);
+        System.out.println(shopCategory);
+        System.out.println(isClosed);
+    }
+}
