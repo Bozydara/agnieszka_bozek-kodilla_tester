707d56f0a447cfee5529bbefa98eb62b2085fd59
Agnieszka Bozek <a.bozek90@gmail.com>
2025-01-11 21:05:22 +0100

    Task with leap year

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 1700;
+
+        if (isLeapYear(year)) {
+            System.out.println("This is a leap year!");
+        } else {
+            System.out.println("This is not a leap year!");
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else return year % 4 == 0 && year % 100 != 0;
+    }
+}
