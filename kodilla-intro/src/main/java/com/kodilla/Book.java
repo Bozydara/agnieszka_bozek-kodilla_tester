50c3d198ed1034976b5b805e4ffcecc022ee087a
Agnieszka Bozek <a.bozek90@gmail.com>
2025-02-18 07:55:33 +0100

    Add Book class

---
+++ kodilla-intro/src/main/java/com/kodilla/Book.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class Book {
+    private String author;
+    private String title;
+
+    public static Book of(String author, String title) {
+        Book book = new Book();
+        book.title = title;
+        book.author = author;
+        return book;
+    }
+}
