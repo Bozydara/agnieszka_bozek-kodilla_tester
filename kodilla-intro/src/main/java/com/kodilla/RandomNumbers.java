cea3c24be9dfaf28077fd9d879c448f76f13d8f1
Agnieszka Bozek <a.bozek90@gmail.com>
2025-02-21 16:47:40 +0100

    Add RandomNumbers class

---
+++ kodilla-intro/src/main/java/com/kodilla/RandomNumbers.java
@@ -0,0 +1,62 @@
+package com.kodilla;
+
+import java.util.Random;
+
+public class RandomNumbers {
+
+
+    public static void sumRandomNumbers() {
+        Random random = new Random();
+        int sum = 0;
+        while (sum < 5000) {
+            int randomNumber = random.nextInt(31);
+            sum = sum + randomNumber;
+        }
+    }
+
+    public static int getTheLowestRandomNumber() {
+        Random random = new Random();
+        int sum = 0;
+        int previousRandomNumber = 0;
+        int theLowestNumber = 0;
+        while (sum < 5000) {
+            int randomNumber = random.nextInt(31);
+            sum = sum + randomNumber;
+            if (randomNumber < previousRandomNumber || previousRandomNumber == 0) {
+                if (randomNumber < theLowestNumber || theLowestNumber == 0) {
+                    theLowestNumber = randomNumber;
+                    if (theLowestNumber == 0) {
+                        return theLowestNumber;
+                    }
+                }
+            }
+            previousRandomNumber = 0 + randomNumber;
+        }
+        return theLowestNumber;
+    }
+
+    public static int getTheHighestRandomNumber() {
+        Random random = new Random();
+        int sum = 0;
+        int theHighestNumber = 0;
+        int previousRandomNumber = 0;
+        while (sum < 5000) {
+            int randomNumber = random.nextInt(31);
+            sum = sum + randomNumber;
+
+            if (randomNumber > previousRandomNumber) {
+                if (randomNumber > theHighestNumber) {
+                    theHighestNumber = randomNumber;
+                }
+                if (theHighestNumber == 30) {
+                    return theHighestNumber;
+                }
+            }
+            previousRandomNumber = 0 + randomNumber;
+        }
+        return theHighestNumber;
+    }
+}
+
+
+
