6a51e0c8eff7f8431b191b962729d00ca4fe6afd
Agnieszka Bozek <a.bozek90@gmail.com>
2025-04-15 14:39:01 +0200

    Correct car factory homework v2

--- kodilla-spring-basic/src/main/java/com/kodilla/basic/spring_configuration/homework/Car.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/basic/spring_configuration/homework/Car.java
@@ -1,20 +1,15 @@
 package com.kodilla.basic.spring_configuration.homework;
 
 import java.time.LocalTime;
-import java.util.Random;
 
 public interface Car {
 
     default boolean hasHeadlightsTurnedOn() {
 
-        Random generator = new Random();
-        int hour = generator.nextInt(24);
-        int minute = generator.nextInt(61);
+        LocalTime localTime = LocalTime.now();
 
-        LocalTime randomTime = LocalTime.of(hour, minute);
-
-        if (randomTime.equals(LocalTime.of(06, 00)) || randomTime.isAfter(LocalTime.of(06, 00))
-                && randomTime.isBefore(LocalTime.of(20, 00))) {
+        if (localTime.equals(LocalTime.of(06, 00)) || localTime.isAfter(LocalTime.of(06, 00))
+                && localTime.isBefore(LocalTime.of(20, 00))) {
             {
                 return false;
             }
